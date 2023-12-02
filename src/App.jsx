d4612d9e4c7fb5415a12970da466aeea6d39e663
Eric Zhou <eric.m.zhou@gmail.com>
2023-12-02 01:42:15 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import './App.css';
 
+function Polaroid({ src }) {
+  return (
+    <div className="polaroidOuter">
+      <div className="polaroidInner">
+        <img className="polaroidImg" src={src} />
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <Polaroid src="Octocat.png"></Polaroid>
     </div>
   );
 }
